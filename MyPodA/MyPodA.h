096bf8ff016425ccceeb1d1fb7cbbefa8a108806
Sergii Tatsii <sergiitatsii@gmail.com>
2018-12-13 19:07:51 +0300

    Initial Commit

---
+++ MyPodA/MyPodA.h
@@ -0,0 +1,19 @@
+//
+//  MyPodA.h
+//  MyPodA
+//
+//  Created by Sergii Tatsii on 12/13/18.
+//  Copyright © 2018 Sergii Tatsii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyPodA.
+FOUNDATION_EXPORT double MyPodAVersionNumber;
+
+//! Project version string for MyPodA.
+FOUNDATION_EXPORT const unsigned char MyPodAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyPodA/PublicHeader.h>
+
+
